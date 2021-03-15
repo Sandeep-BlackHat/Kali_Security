cb7e6938e428c33c20383348fd634d4a31030528
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-03-15 20:50:38 +0530

    Update usage.sh

--- EXIF/usage.sh
+++ EXIF/usage.sh
@@ -1,2 +1,5 @@
 simple command to run and exif tools is:
-  root@kali:/$ exif <name of the file>
+  root@kali:/$ exif example.png
+
+Note:
+1. You can use any image extension ranging from jpeg -> png
