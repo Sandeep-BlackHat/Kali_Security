8acc0db091c86801d24581914c73463c260cb406
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-03-15 20:52:58 +0530

    Create command.sh

---
+++ rkhunter/command.sh
@@ -0,0 +1,3 @@
+Simple line to run a rkhunter tools for checking that any rootkit or any malware or any suspecious things are working or any infilteration is done to get the credentials:
+
+root@kali:/$ rkahunter -c
