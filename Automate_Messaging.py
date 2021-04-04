c96691fe8353c0e97c2f3cec980bd2c0329942bc
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-04-04 15:31:13 +0530

    Add files via upload

---
+++ Automate_Messaging.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+while True:
+    time.sleep(0.5)  # for this much time the program will sleep
+    pyautogui.typewrite('Vote for Ultimate Gay Vote for Ultimate Gay TejasTejas Don')  # this will write a message wherever your cursor is.
+    pyautogui.press('enter') # this will press enteridfj
